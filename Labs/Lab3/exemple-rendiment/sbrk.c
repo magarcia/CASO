6e0f939899309a21e0af86e16ab54d4d19a07d34
Martin Garcia <newluxfero@gmail.com>
2014-10-20 00:48:43 +0200

    init with sbrk

---
+++ Labs/Lab3/exemple-rendiment/sbrk.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define    N    1000000
+
+void main () {
+
+    // Define vars
+    unsigned int i;
+    void *p;
+    struct timeval start, end;
+
+    gettimeofday(&start, NULL); // Get start time
+
+    for(i=0; i < N; i++) {
+        p = sbrk((intptr_t) 0);
+
+        if (p == (void *) -1) {
+            printf("Error on execute sbrk");
+            exit(1);
+        }
+    }
+
+    gettimeofday(&end, NULL); // Get end time
+
+    // Get elapset time seconds
+    long t = (end.tv_sec*1e6 + eAnd.tv_usec) - (start.tv_sec*1e6 + start.tv_usec);
+    double time = (double)t/1e6;
+
+    printf("%0.5f s (%0.5f s/sbrk)\n", time, time/N);
+}
