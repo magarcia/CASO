2ab4f3101fe6079ce8f1d53934a9299836c011f4
Martin Garcia <newluxfero@gmail.com>
2014-10-21 11:49:08 -0700

    WIP random :shit:

--- Labs/Lab2/runHurd.sh
+++ Labs/Lab2/runHurd.sh
@@ -2,4 +2,5 @@
 
 qemu-system-i386 -m 512 -net nic,model=rtl8139 -net user \
     -net user,hostfwd=tcp::5555-:22 \
-    -drive cache=writeback,index=0,media=disk,file=$(echo debian-hurd-*.img)
+    -drive cache=writeback,index=0,media=disk,file=$(echo debian-hurd-*.img) \
+    -curses
